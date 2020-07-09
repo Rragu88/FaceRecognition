41acd28958d103f6109b0bae03c483e7c25ab0d8
Ryan <ryragusa@gmail.com>
2020-07-09 13:34:53 -0500

    removed ports from fetch statements

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register  extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://protected-island-67426.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
